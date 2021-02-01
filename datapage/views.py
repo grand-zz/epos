bc2aaf0b3da64e5cd53b30496cca7c491ae29482
MC <aa@11>
2021-02-01 11:47:55 +0800

    page.render

--- datapage/views.py
+++ datapage/views.py
@@ -15,6 +15,11 @@ from pyecharts.components import Table
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 import pandas as pd
+from jinja2 import Environment, FileSystemLoader
+from pyecharts.globals import CurrentConfig
+
+# CurrentConfig.GLOBAL_ENV = Environment(loader=FileSystemLoader("/templates"))
+
 
 def table_base(sql,titl) -> Table:
     # for arg in args:
@@ -52,7 +57,14 @@ def Pie_base(sql,titl) -> Pie:
     pie.set_series_opts(label_opts=opts.LabelOpts(formatter="{b}: {d}%"))
     return pie
 
-def page_default_layout(rq1, rq2, rq3):
+def chart(request):
+    request.encoding = 'utf-8'
+    if 'rq1' in request.GET and request.GET['rq1']:
+        # message = '你搜索的内容为: ' + request.GET['rq1']
+        rq1 = request.GET['rq1']
+        rq2 = request.GET['rq2']
+        rq3 = request.GET['rq3']
+
     sql1 = '''select pwh as 铺位号,pp as 品牌,gzlx as 故障类型,操作,软件,硬件,其它,操作+软件+硬件+其它 as 合计,rq as 日期,ms as 描述,clr as 处理人 from (select pwh,gzlx,pp,COUNT(if(gzlx='操作',true,null)) as 操作,COUNT(if(gzlx='软件',true,null)) as 软件,COUNT(if(gzlx='硬件',true,null)) as 硬件,COUNT(if(gzlx='其它',true,null)) as 其它,rq,ms,clr from b_epos where rq BETWEEN '%s' and '%s' group by pwh,gzlx,pp,rq,ms,clr) c order by rq,pwh''' % (
         rq1, rq2)
     titl1 = "POS前台收银问题处理汇总\n\n查询日期：%s--%s" % (rq1, rq2)
@@ -90,20 +102,10 @@ def page_default_layout(rq1, rq2, rq3):
         table_base(sql3, titl3),
         table_base(sql4, titl4),
     )
-    page.render('templates/datapage/page_simple_layout.html')
+    return HttpResponse(page.render_embed())
 
 
-def chart(request):
-    request.encoding = 'utf-8'
-    if 'rq1' in request.GET and request.GET['rq1']:
-        # message = '你搜索的内容为: ' + request.GET['rq1']
-        rq1 = request.GET['rq1']
-        rq2 = request.GET['rq2']
-        rq3 = request.GET['rq3']
 
-    page_default_layout(rq1,rq2,rq3)
-
-    return render(request, 'datapage/page_simple_layout.html')
 
 def index(request):
     return render(request, 'datapage/index.html')
